df7939c5035e73bedd198d4b229892b4e6bd2e09
HongDeng-Git <denghong123419@gmail.com>
2023-02-10 00:00:53 +0100

    fix the extra straight length

--- test/directional_coupler.py
+++ test/directional_coupler.py
@@ -55,31 +55,22 @@ class DC(PlaceAndConnect):
     _name_prefix = "DC"
     
     Straight_DC = i3.ChildCellProperty(doc="coupler")
-    wg_in1 = i3.ChildCellProperty()
-    wg_in2 = i3.ChildCellProperty()
-    wg_out1 = i3.ChildCellProperty()
-    wg_out2 = i3.ChildCellProperty()
+    wg = i3.ChildCellProperty()
     trace_template = i3.ChildCellProperty(doc="used trace template")
     
     def _default_Straight_DC(self):
         return StraightDC(trace_template = self.trace_template)
-    def _default_wg_in1(self):
-        return i3.RoundedWaveguide(name = "trace_in1", trace_template = self.trace_template)
-    def _default_wg_in2(self):
-        return i3.RoundedWaveguide(name = "trace_in2", trace_template = self.trace_template)
-    def _default_wg_out1(self):
-        return i3.RoundedWaveguide(name = "trace_out1", trace_template = self.trace_template)
-    def _default_wg_out2(self):
-        return i3.RoundedWaveguide(name = "trace_out2", trace_template = self.trace_template)
+    def _default_wg(self):
+        return i3.RoundedWaveguide(name = "trace", trace_template = self.trace_template)
     def _default_trace_template(self):
         return WireWaveguideTemplate()
     def _default_child_cells(self):
         cc = {}
         cc["dc"] = self.Straight_DC
-        cc["wg_in1"] = self.wg_in1
-        cc["wg_in2"] = self.wg_in1
-        cc["wg_out1"] = self.wg_in1
-        cc["wg_out2"] = self.wg_in1
+        cc["wg_in1"] = self.wg
+        cc["wg_in2"] = self.wg
+        cc["wg_out1"] = self.wg
+        cc["wg_out2"] = self.wg
         
         return cc
     def _default_links(self):
@@ -101,11 +92,13 @@ class DC(PlaceAndConnect):
     class Layout(PlaceAndConnect.Layout):
         coupling_length = i3.NonNegativeNumberProperty(doc="straight_coupling_length")
         coupling_gap = i3.PositiveNumberProperty(doc="Center_to_center distence")
-        
+        coupl_length = i3.NonNegativeNumberProperty(doc="reduce 0.01 um for connection")
         def _default_coupling_length(self):
             return 15.0
         def _default_coupling_gap(self):
             return 0.68
+        def _default_coupl_length(self):
+            return self.coupling_length - 0.01
         
         def _default_trace_template(self):
             tt = self.cell.trace_template.get_default_view(self)
@@ -113,53 +106,34 @@ class DC(PlaceAndConnect):
             return tt
         def _default_Straight_DC(self):
             temp = self.cell.Straight_DC.get_default_view(self)
-            temp.set(coupling_length = self.coupling_length,
+            temp.set(coupling_length = self.coupl_length,   
                      coupling_gap = self.coupling_gap)
             return temp        
-        def _default_wg_in1(self):
-            temp = self.cell.wg_in1.get_default_view(self)
+        def _default_wg(self):
+            temp = self.cell.wg.get_default_view(self)
             temp.set(manhattan=True,
-                     rounding_algorithm=i3.SplineRoundingAlgorithm(), 
+                     rounding_algorithm=i3.SplineRoundingAlgorithm(),
+                    
                      bend_radius=10.0,
-                     shape=[(-5, 0), (0, 0), (6, -6), (11, -6)], 
+                     shape=[(-5+0.855, 0), (0, 0), (6, -6), (11, -6)], 
             )
-            return temp 
-        def _default_wg_in2(self):
-            temp = self.cell.wg_in2.get_default_view(self)
-            temp.set(manhattan=True,
-                     rounding_algorithm=i3.SplineRoundingAlgorithm(), 
-                     bend_radius=10.0,
-                     shape=[(-20, 0), (0, 0), (0, 20), (20, 20)], 
-            )
-            return temp 
-        def _default_wg_out1(self):
-            temp = self.cell.wg_out1.get_default_view(self)
-            temp.set(manhattan=True,
-                     rounding_algorithm=i3.SplineRoundingAlgorithm(), 
-                     bend_radius=10.0,
-                     shape=[(-20, 0), (0, 0), (0, -20), (20, -20)],
-                     )
             return temp
-        def _default_wg_out2(self):
-            temp = self.cell.wg_out2.get_default_view(self)
-            temp.set(manhattan=True,
-                     rounding_algorithm=i3.SplineRoundingAlgorithm(), 
-                     bend_radius=10.0,
-                     shape=[(-20, 0), (0, 0), (0, 20), (20, 20)],
-                     )
-            return temp
+        
+        def _default_min_straight(self):
+            return 0
+       
         def _default_child_transformations(self):
             
-            wg_in_x = self.wg_in1.ports["in"].x
-            wg_in_y = self.wg_in1.ports["in"].y
-            wg_out_x = self.wg_in1.ports["out"].x
-            wg_out_y = self.wg_in1.ports["out"].y             
+            wg_in_x = self.wg.ports["in"].x
+            wg_in_y = self.wg.ports["in"].y
+            wg_out_x = self.wg.ports["out"].x
+            wg_out_y = self.wg.ports["out"].y
             
             return {"dc": (0, 0),
-                    "wg_in1": i3.HMirror()+ i3.Translation(translation=(+wg_in_x-self.coupling_length/2, -self.coupling_gap/2)) ,
-                    "wg_in2": i3.HMirror()+ i3.VMirror() + i3.Translation(translation=(+wg_in_x-self.coupling_length/2, +self.coupling_gap/2)) ,
-                    "wg_out1": i3.Translation(translation=(-wg_in_x+self.coupling_length/2, -self.coupling_gap/2)),
-                    "wg_out2": i3.VMirror() +i3.Translation(translation=(-wg_in_x+self.coupling_length/2, +self.coupling_gap/2)),
+                    "wg_in1": i3.HMirror()+ i3.Translation(translation=(+wg_in_x-self.coupl_length/2, -self.coupling_gap/2)) ,
+                    "wg_in2": i3.HMirror()+ i3.VMirror() + i3.Translation(translation=(+wg_in_x-self.coupl_length/2, +self.coupling_gap/2)) ,
+                    "wg_out1": i3.Translation(translation=(-wg_in_x+self.coupl_length/2, -self.coupling_gap/2)),
+                    "wg_out2": i3.VMirror() +i3.Translation(translation=(-wg_in_x+self.coupl_length/2, +self.coupling_gap/2)),
                     }
     
     
